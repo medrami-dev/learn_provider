91aa0e9f6c48576b0bc6f3fda490d86d4c9f9745
Mohamed Rami <rami63879@gmail.com>
2023-12-17 11:04:02 +0100

    ChangeNotifier + Consumer + notifyListeners

--- lib/View/home_page.dart
+++ lib/View/home_page.dart
@@ -1,17 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home"),
-      ),
-      body: const Center(
-        child: Text("Hello World"),
-      ),
-    );
+    return ChangeNotifierProvider(
+        create: (context) => Model(),
+        child: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: const Text(
+              "Home",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          body: Consumer<Model>(
+            builder: (context, value, child) {
+              return Center(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(value.name),
+                      ElevatedButton(
+                          onPressed: () {
+                            value.changeName();
+                          },
+                          child: const Text("Do Somthing"))
+                    ]),
+              );
+            },
+          ),
+        ));
+  }
+}
+
+class Model extends ChangeNotifier {
+  String name = "Mohamed";
+  changeName() {
+    name = "ali";
+    notifyListeners();
   }
 }
